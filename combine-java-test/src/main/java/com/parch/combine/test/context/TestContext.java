d8fb35cf6e7f6a64590130bf4322cc64e486b335
潘诚 <p568866418@163.com>
2024-05-16 18:53:45 +0800

    架构调整

--- combine-java-test/src/main/java/com/parch/combine/test/context/TestContext.java
+++
@@ -1,7 +0,0 @@
-package com.parch.combine.test.context;
-
-import com.parch.combine.core.component.service.ICombineJavaService;
-
-public class TestContext {
-    public static ICombineJavaService SERVICE;
-}
