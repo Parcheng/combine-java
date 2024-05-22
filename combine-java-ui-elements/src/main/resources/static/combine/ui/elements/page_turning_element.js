cc5a9bd23c3baf019409dcde2128c1e2ee24c62c
潘诚 <p568866418@163.com>
2024-05-22 20:00:12 +0800

    UI元素样式

--- combine-java-ui-elements/src/main/resources/static/combine/ui/elements/page_turning_element.js
+++ combine-java-ui-elements/src/main/resources/static/combine/ui/elements/page_turning_element.js
@@ -10,11 +10,11 @@ $combine.element.register("SYSTEM.PAGE_TURNING", (function () {
         return instance;
     }
 
-    function buildPage(instance, settings, buildData) {
+    function buildPage(instance, buildData) {
         const body = [];
 
-        const currPage = Number(dataFns.parseVariable(settings.currPage, buildData));
-        const maxPage = Number(dataFns.parseVariable(settings.maxPage, buildData));
+        const currPage = Number(dataFns.parseVariable(instance.currPage, buildData));
+        const maxPage = Number(dataFns.parseVariable(instance.maxPage, buildData));
         if (isNaN(maxPage) || isNaN(currPage)) {
             return domFns.build(instance.template.pageTurning, body);
         }
@@ -26,13 +26,13 @@ $combine.element.register("SYSTEM.PAGE_TURNING", (function () {
         if (currPage <= 1) {
             body.push(buildItem(instance.id, instance.template.lastDisabled, instance.template.lastContent, instance.template.lastContent.text));
         } else {
-            body.push(buildItem(instance.id, instance.template.last, instance.template.lastContent, instance.template.lastContent.text, settings.triggers, buildData, true, currPage - 1));
+            body.push(buildItem(instance.id, instance.template.last, instance.template.lastContent, instance.template.lastContent.text, instance.triggers, buildData, true, currPage - 1));
         }
 
         if (currPage >= maxPage) {
             body.push(buildItem(instance.id, instance.template.nextDisabled, instance.template.nextContent, instance.template.nextContent.text));
         } else {
-            body.push(buildItem(instance.id, instance.template.next, instance.template.nextContent, instance.template.nextContent.text, settings.triggers, buildData, true, currPage + 1));
+            body.push(buildItem(instance.id, instance.template.next, instance.template.nextContent, instance.template.nextContent.text, instance.triggers, buildData, true, currPage + 1));
         }
 
         return domFns.build(instance.template.pageTurning, body);
@@ -40,7 +40,7 @@ $combine.element.register("SYSTEM.PAGE_TURNING", (function () {
 
     function buildItem(id, item, content, body, triggers, buildData, hasChecked, targetPage) {
         const itemDom = domFns.build(item, domFns.build(content, body));
-        if (hasChecked && hasChecked == true) {
+        if (hasChecked && hasChecked === true) {
             domFns.appendProtity(itemDom, "onclick", elementFns.buildCallFnCode(id, "checked", targetPage));
         }
         if (triggers) {
@@ -52,13 +52,13 @@ $combine.element.register("SYSTEM.PAGE_TURNING", (function () {
     return {
         build: function build(instance, data) {
             instance = init(instance, data);
-            return domFns.build(instance.template.external, buildPage(instance, instance.template.settings, data));
+            return domFns.build(instance.template.external, buildPage(instance, data));
         },
         refresh: function refresh(id, instance, parentData) {
             instance = init(instance, parentData);
             let externalDom = document.getElementById(id);
             if (externalDom) {
-                domFns.setBody(externalDom, buildPage(instance, instance.template.settings, parentData));
+                domFns.setBody(externalDom, buildPage(instance, parentData));
             }
         },
         getData: function getData(id) {
