cf2869ccd4e0644316650c370f651d3712d7d0a4
Parch <p568866418@163.com>
2024-08-28 15:25:33 +0800

    编辑器

--- combine-java-spring-web/src/main/resources/editor/editor.js
+++ combine-java-spring-web/src/main/resources/editor/editor.js
@@ -2,10 +2,39 @@
 // 引用暂时只允许引block的
 // flow-component，flow-item 构建 取值 序列化 封装成对象，可new
 
+var groupSettings = {};
+var componentSettings = {};
+
+var config = { init: {}, block: {}, before: {}, after: {}, flow: {} };
+
+// document.getElementsById("group");
+
 var buildTool = {
-    groups: function() {
+    groups: function(data) {
+        var doms = [];
+        for (let index = 0; index < data.length; index++) {
+            var itemData = data[i];
+            var itemDom = document.createElement("div");
+            itemDom.id = "g-" + itemData.key;
+            itemDom.className = "item";
+            itemDom.textContent = itemData.name;
+            doms.push(itemDom);
+        }
+
+        return doms;
     },
-    components: function() {
+    components: function(parentKey, data) {
+        var doms = [];
+        for (let index = 0; index < data.length; index++) {
+            var itemData = data[i];
+            var itemDom = document.createElement("div");
+            itemDom.id = "c-" + parentKey + "." + itemData.key;
+            itemDom.className = "item";
+            itemDom.textContent = itemData.name;
+            doms.push(itemDom);
+        }
+
+        return doms;
     },
     componentSettings: function() {
     },
@@ -15,4 +44,4 @@ var buildTool = {
     },
     flowSettings: function() {
     }
-}
+};
