1627e73571d4b50eeaf1a977d4fde0261adcbece
Parch <p568866418@163.com>
2024-09-02 12:10:34 +0800

    编辑器

--- combine-java-spring-web/src/main/resources/editor/editor.js
+++ combine-java-spring-web/src/main/resources/editor/editor.js
@@ -9,39 +9,149 @@ var config = { init: {}, block: {}, before: {}, after: {}, flow: {} };
 
 // document.getElementsById("group");
 
-var buildTool = {
-    groups: function(data) {
-        var doms = [];
-        for (let index = 0; index < data.length; index++) {
-            var itemData = data[i];
-            var itemDom = document.createElement("div");
-            itemDom.id = "g-" + itemData.key;
-            itemDom.className = "item";
-            itemDom.textContent = itemData.name;
-            doms.push(itemDom);
-        }
 
-        return doms;
-    },
-    components: function(parentKey, data) {
-        var doms = [];
-        for (let index = 0; index < data.length; index++) {
-            var itemData = data[i];
-            var itemDom = document.createElement("div");
-            itemDom.id = "c-" + parentKey + "." + itemData.key;
-            itemDom.className = "item";
-            itemDom.textContent = itemData.name;
-            doms.push(itemDom);
-        }
+var buildFns = {
 
-        return doms;
+}
+
+var buildDomFns = {
+    tool: {
+        groups: function(data) {
+            var doms = [];
+            for (let index = 0; index < data.length; index++) {
+                var itemData = data[i];
+                var key = itemData.key;
+                var itemDom = document.createElement("div");
+                itemDom.id = "g-" + key;
+                itemDom.className = "item";
+                itemDom.textContent = itemData.name;
+                itemDom.onclick = function() {
+                    callFns.tool.checkGroup(key);
+                }
+                doms.push(itemDom);
+            }
+    
+            return doms;
+        },
+        components: function(parentKey, data) {
+            var doms = [];
+            for (let index = 0; index < data.length; index++) {
+                var itemData = data[i];
+                var key = itemData.key;
+                var itemDom = document.createElement("div");
+                itemDom.id = "c-" + parentKey + "." + key;
+                itemDom.className = "item";
+                itemDom.textContent = itemData.name;
+                itemDom.onclick = function() {
+                    callFns.tool.openCheckComponentWindow(key);
+                }
+                doms.push(itemDom);
+            }
+    
+            return doms;
+        },
     },
-    componentSettings: function() {
+    node: {
+        componentInit: function(initId, data) {
+            var dom = document.createElement("div");
+            dom.id = "cin-" + data.key;
+            dom.className = "component-item";
+            dom.innerHTML = data.type + "<br>" + data.key;
+            dom.ondblclick = function() {
+                callFns.node.openComponentInitWindow(initId);
+            }
+            return dom;
+        },
+        componentLogic: function(logicId, data) {
+            var dom = document.createElement("div");
+            dom.name = "cn-" + data.key;
+            dom.className = "component-item";
+            dom.innerHTML = data.type + "<br>" + data.key;
+            dom.ondblclick = function() {
+                callFns.node.openComponentLogicWindow(logicId);
+            }
+            return dom;
+        },
+        beforeOrAfterFlow: function(flowId) {
+            var dom = document.createElement("div");
+            dom.id = "boaf-" + flowId;
+            dom.className = "flow-item";
+            return dom;
+        },
+        flow: function(flowId) {
+            var dom = document.createElement("div");
+            dom.id = "f-" + flowId;
+            dom.className = "flow-item";
+            return dom;
+        },
+        flowSettingsItem: function(flowId) {
+            var dom = document.createElement("div");
+            dom.className = "settings-item";
+            dom.textContent = "设置";
+            dom.ondblclick = function() {
+                callFns.node.openFlowSettingsWindow(flowId);
+            }
+            return dom;
+        },
+        flowLineItem: function() {
+            var dom = document.createElement("div");
+            dom.className = "line-item";
+            dom.textContent = "→";
+            return dom;
+        },
+        flowPathItem: function(flowId, flowPath) {
+            var dom = document.createElement("div");
+            dom.className = "path-item";
+            dom.textContent = flowPath;
+            dom.ondblclick = function() {
+                callFns.node.openFlowPathWindow(flowId);
+            }
+            return dom;
+        }
     },
-    flowPath: function() {
-    },
-    flowLine: function() {
-    },
-    flowSettings: function() {
+    window: {
+
     }
 };
+
+var callFns = {
+    tool: {
+        checkGroup: function(key) {
+        },
+
+        openCheckComponentWindow: function(key) {
+        },
+        confirmCheckComponent: function(key) {
+        }
+    },
+    node: {
+        openComponentInitWindow: function(initId) {
+        },
+        confirmComponentInit: function() {
+        },
+        
+        openComponentLogicWindow: function(logicId) {
+        },
+        confirmComponentLogic: function(flowId) {
+        },
+
+        openFlowSettingsWindow: function(flowId) {
+        },
+        confirmFlowSettings: function() {
+        },
+
+        openFlowPathWindow: function(flowId) {
+        },
+        confirmFlowPath: function(flowId) {
+        }
+    },
+    window: {
+        close: function(domId) {
+            var dom = document.getElementById(domId);
+            if (dom && dom.parentNode) {
+                dom.parentNode.removeChild(dom);
+            }
+        }
+    }
+
+}
