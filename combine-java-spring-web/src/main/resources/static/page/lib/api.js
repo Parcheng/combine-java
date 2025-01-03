afc0f69580ce629403fb050e9b034c3182bdd06f
Parch <p568866418@163.com>
2025-01-03 17:01:29 +0800

    API

--- combine-java-spring-web/src/main/resources/static/page/lib/api.js
+++ combine-java-spring-web/src/main/resources/static/page/lib/api.js
@@ -74,18 +74,87 @@ const initFns = {
         componentMenuFns.config.groupDomId = "config-group";
         componentMenuFns.config.componentDomId = "config-component";
         componentMenuFns.opt.checkComponent = function(key) {
-            console.log(key);
-            // var checkKeyDom = document.getElementById("check-board-source-key");
-            // checkKeyDom.value = key;
-        
-            // var boardSelectDom = document.getElementById("check-board-select");
-            // boardSelectDom.dispatchEvent(new Event("change"));
-        
-            // var boardDom = document.getElementById("check-board-window");
-            // domTools.switchDisplay(boardDom, true);
-            // window.scrollTo(0, 0);
+            const currComponent = componentMap[key];
+            if (!currComponent) {
+                return;
+            }
+            
+            const configInitDom = document.getElementById("config-init-content");
+            const initConfigDoms = buildFns.fieldItems(currComponent.initConfig);
+            domTools.setAll(configInitDom, initConfigDoms);
+            
+            const configLogicDom = document.getElementById("config-logic-content");
+            const logicConfigDoms = buildFns.fieldItems(currComponent.logicConfig);
+            domTools.setAll(configLogicDom, logicConfigDoms);
         }
 
         componentMenuFns.init.groups();
     },
+}
+
+const buildFns = {
+    fieldItems: function(configs) {
+        if (!configs || configs.length == 0) {
+            return buildFns.emptyItems();
+        }
+
+        const body = [];
+        for (let i = 0; i < configs.length; i++) {
+            const itemDom = buildDomFns.item(configs[i]);
+            body.push(itemDom);
+        }
+
+        return body;
+    },
+    emptyItems: function() {
+        var dom = document.createElement("div");
+        dom.className = "item";
+        dom.textContent = "暂无配置"
+        return dom;
+    }
+}
+
+const buildDomFns = {
+    item: function(config) {
+        var dom = document.createElement("div");
+        dom.className = "item";
+
+        var headerSpan = document.createElement("span");
+        headerSpan.textContent = config.key + 
+            " | " + config.name + " | " + config.type + 
+            " | " + (config.isArray ? "数组 | " : "非数组 | ") + 
+            " | " + (config.isRequired ? "必填 | " : "非必填")
+        dom.appendChild(headerSpan);
+
+        if (config.desc && config.desc.length > 0) {
+            for (let di = 0; di < config.desc.length; di++) {
+                const descItemSpan = document.createElement("span");
+                descItemSpan.textContent = config[di];
+                dom.appendChild(descItemSpan);
+            }
+        }
+        
+        if (config.egs && config.egs.length > 0) {
+            if (config.egs.length > 1) {
+                const egTitleSpan = document.createElement("span");
+                egTitleSpan.textContent = "示例: ";
+                dom.appendChild(egTitleSpan);
+            } else {
+                const egFirstSpan = document.createElement("span");
+                egFirstSpan.textContent = "示例: " + config[0];
+                dom.appendChild(egFirstSpan);
+
+                for (let ei = 1; ei < config.egs.length; ei++) {
+                    const egItemSpan = document.createElement("span");
+                    egItemSpan.textContent = config[di];
+                    dom.appendChild(egItemSpan);
+                }
+            }
+        }
+
+        return dom;
+    },
+    subItem: function() {
+
+    }
 }
