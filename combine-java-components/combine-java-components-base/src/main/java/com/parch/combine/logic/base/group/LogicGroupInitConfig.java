aea12a83c4fcb141b5fe4e55c1e4fa807dd3c84a
潘诚 <p568866418@163.com>
2025-01-16 22:26:04 +0800

    Group组件

---
+++ combine-java-components/combine-java-components-base/src/main/java/com/parch/combine/logic/base/group/LogicGroupInitConfig.java
@@ -0,0 +1,8 @@
+package com.parch.combine.logic.base.group;
+
+import com.parch.combine.core.common.settings.annotations.Invalid;
+import com.parch.combine.core.component.base.IInitConfig;
+
+@Invalid
+public interface LogicGroupInitConfig extends IInitConfig {
+}
---
+++ combine-java-components/combine-java-components-base/src/main/java/com/parch/combine/logic/components/LogicGroupComponent.java
@@ -0,0 +1,36 @@
+package com.parch.combine.logic.components;
+
+import com.parch.combine.core.common.util.CheckEmptyUtil;
+import com.parch.combine.core.component.base.AbstractComponent;
+import com.parch.combine.core.component.settings.annotations.Component;
+import com.parch.combine.core.component.settings.annotations.ComponentResult;
+import com.parch.combine.core.component.tools.SubComponentTool;
+import com.parch.combine.core.component.tools.variable.DataVariableHelper;
+import com.parch.combine.core.component.vo.ComponentDataResult;
+import com.parch.combine.logic.base.group.LogicGroupInitConfig;
+import com.parch.combine.logic.base.group.LogicGroupLogicConfig;
+
+@Component(key = "group", name = "组件组封装组件", logicConfigClass = LogicGroupLogicConfig.class, initConfigClass = LogicGroupInitConfig.class)
+@ComponentResult(name = "组执行结果")
+public class LogicGroupComponent extends AbstractComponent<LogicGroupInitConfig, LogicGroupLogicConfig> {
+
+    public LogicGroupComponent() {
+        super(LogicGroupInitConfig.class, LogicGroupLogicConfig.class);
+    }
+
+    @Override
+    public ComponentDataResult execute() {
+        String[] componentIds = getLogicConfig().components();
+        ComponentDataResult result = SubComponentTool.execute(manager, componentIds);
+        if (result.getSuccess()) {
+            String out = getLogicConfig().out();
+            if (CheckEmptyUtil.isEmpty(out)) {
+                return ComponentDataResult.success(result);
+            } else {
+                return ComponentDataResult.success(DataVariableHelper.parseValue(out, true));
+            }
+        }
+
+        return ComponentDataResult.success(result);
+    }
+}
